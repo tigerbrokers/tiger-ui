0a4ca8593fa38bfdcf28f5e9c9f2330083c10810
ijse <i@ijser.cn>
2017-08-05 18:30:02 +0800

    fix maxium error on updating page title

--- app.js
+++ app.js
@@ -9,5 +9,7 @@ document.querySelector('.sidebar h1').innerText = 'Tiger-UI Playground'
 document.querySelector('title')
   .addEventListener('DOMSubtreeModified', evt => {
     let title = evt.target.innerText
-    document.title = title.replace('Vue Play', 'Tiger-UI')
+    if (/Vue Play/.test(title)) {
+      document.title = title.replace('Vue Play', 'Tiger-UI')
+    }
   })
