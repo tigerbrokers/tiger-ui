7748bf5165d005cc1337761fa2ad8d33c519827f
Li Yi <i@ijser.cn>
2017-08-23 12:44:38 +0800

    Feature webpack (#4)
    * remove file
    
    * remove precommit'
    
    * add webpack
    
    * optimize build and add sourcemap
    
    * add test for build
    
    * fix lint

---
+++ index.js
@@ -0,0 +1,21 @@
+const files = require.context('.', true, /^(?!\.\/node_modules|\.\/docs).*\/index\.vue$/)
+const modules = []
+
+files.keys().forEach((key) => {
+  modules.push(files(key).default)
+})
+
+const pkg = require('./package.json')
+
+// export as vue plugin
+export default {
+  list: modules,
+  version: pkg.version,
+  install (Vue) {
+    modules.forEach(mod => {
+      Vue.component(mod.name, mod)
+    })
+  }
+}
+
+export const list = modules
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'tiger-ui.js',
+    library: 'TigerUI',
+    libraryTarget: 'umd',
+    sourceMapFilename: 'tiger-ui.js.map',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'cheap-source-map',
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }, {
+      test: /\.vue$/,
+      loader: 'vue-loader',
+      options: {}
+    }, {
+      test: /\.css$/,
+      use: [ 'style-loader', 'css-loader' ]
+    }]
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    })
+  ]
+}
